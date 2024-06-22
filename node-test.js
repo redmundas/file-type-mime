b30e877df70902d6414b70520cd69bb9d9c43ffc
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2024-06-22 13:54:08 +0300

    Minor build improvements (#228)

--- node-test.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { parse } from './packages/file-type-mime/dist/index.mjs';
-
-const file = resolve('./packages/file-type-mime/data/sample.deb');
-const buffer = readFileSync(file);
-const result = parse(buffer);
-
-console.log('MIME_TYPE', result);
